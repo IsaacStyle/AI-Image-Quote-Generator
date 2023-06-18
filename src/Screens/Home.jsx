53f9210f1fb4ee1825b82ad96ca04e45133e707c
IsaacStyle <isaac.opra@gmail.com>
2023-06-18 19:54:46 -0400

    Ai base code complete

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { Configuration, OpenAIApi } from "openai";
 
 export default function Home() {
+    const [ai,setAI] = useState()
+    const configuration = new Configuration({
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+      });
+    const openai = new OpenAIApi(configuration);
+    const completion = async() => {
+        const complete = await openai.createImage({
+            prompt: "a white siamese cat",
+            n: 1,
+            size: "1024x1024"
+    })
+      setAI(complete.data.data[0].url)
+      return complete.data.data[0].url
+    }
+    const handleClick = () => {
+        completion()
+    }
+    useEffect(() => {
+    },[])
+
     return(
-        <div>Hello World</div>
+        <div><button onClick={handleClick}>Hello</button>
+        <div><img src={ai} alt="hello" /></div>
+        </div>
     )
 
 }
