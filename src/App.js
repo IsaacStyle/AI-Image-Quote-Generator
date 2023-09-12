03c5d0a17882003d3a10183e9f3b3f7481d1ba57
IsaacStyle <isaac.opra@gmail.com>
2023-09-12 11:42:31 -0400

    Canvas tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { Link,Route,Routes } from 'react-router-dom';
-import Home from './Screens/Home';
+// import Home from './Screens/Home';
 import Nav from './Screens/Nav';
+import Home2 from './Screens/Home copy';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Nav />
       <div className='page'>
         <Routes>
-          <Route path={'/'} element={<Home />} />
+          <Route path={'/'} element={<Home2 />} />
         </Routes>
       </div>
     </>
--- src/Screens/Home copy.jsx
+++ src/Screens/Home copy.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 import { Configuration, OpenAIApi } from "openai";
 
-export default function Home() {
+export default function Home2() {
     const [ai,setAI] = useState()
+    const [image, setImage] = useState('');
     const [prompt2,setprompt2] = useState('')
     const [quote2,setQuote2] = useState('')
     const configuration = new Configuration({
@@ -17,12 +18,46 @@ export default function Home() {
             size: "512x512"
     })
       setAI(complete.data.data[0].url)
+      return complete.data.data[0].url
     }
-    const handleClick = (e) => {
-        e.preventDefault();
-        completion()
 
-    }
+    const handleClick = async (e) => {
+      e.preventDefault();
+    
+      // Assuming completion() is an async function, await it
+      await completion();
+    
+      // Load the image from the 'ai' source
+      const img = new Image();
+      img.src = ai;
+      
+      img.onload = () => {
+        // Create a canvas element and get its 2D context
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        
+        // Set canvas dimensions to match the image
+        canvas.width = img.width;
+        canvas.height = img.height;
+        
+        // Draw the image on the canvas
+        context.drawImage(img, 0, 0);
+        
+        // Add text overlay
+        context.font = '30px Arial';
+        context.fillStyle = 'white';
+        context.fillText(quote2, 20, 40); // Adjust text position as needed
+        
+        // Convert the canvas to a data URL with JPEG format
+        const dataURL = canvas.toDataURL('image/jpeg');
+        console.log(img)
+        console.log(dataURL)
+    
+        // Assuming 'setImage' is a function to set the image data URL, you should use it like this:
+        setImage(dataURL);
+      };
+    };
+
     const handleChange = (e) => {
         const { value, name } = e.target;
         setprompt2(value);
@@ -33,14 +68,12 @@ export default function Home() {
         const { value, name } = e.target;
         setQuote2(value);
       };
-    useEffect(() => {
-    },[])
 
     return(
         <div>
         <div className='imageBox'>
-          <img className='aiImage' style={{border:'solid'}} src={ai || "https://i.gifer.com/3H9v.gif"} alt="hello" />
-          <p className='quote'>"Awesome quote goes here lol""Awesome quote goes here lol""Awesome quote goes here lol""Awesome quote goes here lol"</p>
+          <img className='aiImage' style={{border:'solid'}} src={image || "https://i.gifer.com/3H9v.gif"} alt="hello" />
+          {/* <p className='quote'></p> */}
         </div>
         <form className='createForm' onSubmit={handleClick}>
             <input
